3dda77ad048f25a7c13d4ba28d51ad997d7f005a
David Dossot <david@dossot.net>
2015-02-04 14:50:26 -0800

    inced elasticmq

--- project.clj
+++ project.clj
@@ -23,7 +23,7 @@
    [circleci/clj-yaml "0.5.3"]
    [de.ubercode.clostache/clostache "1.4.0"]
 
-   [org.elasticmq/elasticmq-rest-sqs_2.11 "0.8.2"]
+   [org.elasticmq/elasticmq-rest-sqs_2.11 "0.8.5"]
    [amazonica "0.3.13" :exclusions [com.amazonaws/aws-java-sdk
                                     com.amazonaws/amazon-kinesis-client
                                     joda-time
