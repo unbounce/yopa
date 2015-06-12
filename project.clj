87a6b16f4f36835173001a3753a8001f3f6cb671
David Dossot <david@dossot.net>
2015-06-12 08:59:48 -0700

    inced AWS SDK and jRuby

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (def project-version "1.0.0-SNAPSHOT")
 (def build-meta (str "YOPA " project-version " - built on: " (java.util.Date.)))
 (def ring-version "1.3.2")
-(def aws-sdk-version "1.9.40")
+(def aws-sdk-version "1.10.0")
 
 (defproject com.unbounce/yopa project-version
   :description "YOPA is Your Own Personal Aws"
@@ -37,8 +37,8 @@
    [circleci/clj-yaml "0.5.3"]
    [de.ubercode.clostache/clostache "1.4.0"]
 
-   [org.elasticmq/elasticmq-rest-sqs_2.11 "0.8.8"]
-   [amazonica "0.3.23" :exclusions [com.amazonaws/aws-java-sdk]]
+   [org.elasticmq/elasticmq-rest-sqs_2.11 "0.8.8" :exclusions [joda-time]]
+   [amazonica "0.3.24" :exclusions [com.amazonaws/aws-java-sdk]]
    [com.amazonaws/aws-java-sdk-sqs ~aws-sdk-version :exclusions [joda-time]]
    [com.amazonaws/aws-java-sdk-sns ~aws-sdk-version :exclusions [joda-time]]
    [com.amazonaws/aws-java-sdk-s3 ~aws-sdk-version :exclusions [joda-time]]
@@ -46,12 +46,16 @@
    [com.amazonaws/aws-java-sdk-cloudsearch ~aws-sdk-version :exclusions [joda-time]]
    [com.amazonaws/aws-java-sdk-lambda ~aws-sdk-version :exclusions [joda-time]]
 
-   [clj-http "1.1.2"]
+   [clj-http "1.1.2" :exclusions [commons-logging
+                                  cheshire
+                                  org.apache.httpcomponents/httpclient]]
+   [cheshire "5.5.0"]
    [ring/ring-core ~ring-version]
    [ring/ring-jetty-adapter ~ring-version]
 
-   [org.jruby/jruby "1.7.20" :exclusions [com.github.jnr/jffi
-                                          com.github.jnr/jnr-x86asm]]
+   [org.jruby/jruby "9.0.0.0.rc1" :exclusions [com.github.jnr/jffi
+                                               com.github.jnr/jnr-x86asm
+                                               joda-time]]
   ]
 
   :repositories
