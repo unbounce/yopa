cc953217c09251f57831372033b7fb683c1f036d
David Dossot <david@dossot.net>
2015-06-30 14:31:26 -0700

    clojure 1.7.0 \o/

--- project.clj
+++ project.clj
@@ -25,7 +25,7 @@
 
   :dependencies
   [
-   [org.clojure/clojure "1.6.0"]
+   [org.clojure/clojure "1.7.0"]
    [org.clojure/data.xml "0.0.8"]
    [org.clojure/data.json "0.2.6"]
    [org.clojure/tools.cli "0.3.1"]
