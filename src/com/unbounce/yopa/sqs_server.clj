ddc03af1fa426675a1ed46d12b63efe7997b0217
Michael Shannon <mshannon@urbn.com>
2016-05-21 16:01:05 -0400

    SQS Update - Pass Host/Port & Update to 8.12

--- src/com/unbounce/yopa/sqs_server.clj
+++ src/com/unbounce/yopa/sqs_server.clj
@@ -2,11 +2,8 @@
   (:require [amazonica.core :as aws]
             [amazonica.aws.sqs :as sqs]
             [clojure.tools.logging :as log])
-  (:import org.elasticmq.rest.sqs.SQSRestServerBuilder
-           org.elasticmq.server.ElasticMQServer
+  (:import org.elasticmq.server.ElasticMQServer
            org.elasticmq.server.ElasticMQServerConfig
-           org.elasticmq.NodeAddress
-           com.typesafe.config.ConfigObject
            com.typesafe.config.ConfigFactory
            com.typesafe.config.Config))
 
@@ -17,44 +14,39 @@
         \"loglevel\": \"DEBUG\",
         \"logging-filter\": \"akka.event.slf4j.Slf4jLoggingFilter\",
         \"log-dead-letters-during-shutdown\": false
-    }
+    },
    \"akka.http.server.request-timeout\": \"21 s\",
    \"akka.http.server.parsing.max-uri-length\": \"256k\",
    \"storage\":  {
-      \"type\" = \"in-memory\"
+      \"type\": \"in-memory\"
    },
-   \"node-address\":{
-      \"protocol\":\"http\",
-      \"host\":\"0.0.0.0\",
-      \"port\":\"47195\",
-      \"context-path\":\"\"
+   \"node-address\": {
+      \"protocol\": \"http\",
+      \"host\": \"%s\",
+      \"port\": \"%d\",
+      \"context-path\": \"\"
    },
-   \"rest-sqs\":{
-      \"enabled\":\"true\",
-      \"bind-port\":\"47195\",
-      \"bind-hostname\":\"0.0.0.0\",
-      \"sqs-limits\":\"relaxed\"
-   }
+   \"rest-sqs\": {
+      \"enabled\": true,
+      \"bind-hostname\": \"%s\",
+      \"bind-port\": \"%d\",
+      \"sqs-limits\": \"relaxed\"
+   },
+   \"queues\": {}
 }")
 
 (def ^:private ^:dynamic server (atom nil))
 
-(defn- make-main-server []
-  (let [typesafe-config (ConfigFactory/parseString config-string)]
+(defn- make-main-server [host bind-address port]
+  (let [formatted-config (format config-string host port bind-address port)]
+  (log/info formatted-config)
+  (let [typesafe-config (ConfigFactory/parseString formatted-config)]
   (let [config (ElasticMQServerConfig. typesafe-config)]
   (-> (ElasticMQServer. config)
-    (.start)))))
-
-(defn- make-server [host bind-address port]
-  (let [address (NodeAddress. "http", host, port, "")]
-  (-> (SQSRestServerBuilder/withPort port)
-    (.withInterface bind-address)
-    (.withPort port)
-    (.withServerAddress address)
-    (.start))))
+    (.start))))))
 
 (defn start [host bind-address port]
-  (reset! server (make-main-server))
+  (reset! server (make-main-server host bind-address port))
   (log/info (format "Active SQS endpoint: http://%s:%d" bind-address port)))
 
 (defn stop []
